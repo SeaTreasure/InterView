bac170f523020f3675c13fcaac891a65a5200849
王文朋 <wangwenpeng@lianluo.com>
2018-02-27 10:13:26 +0800

    初始化工程

---
+++ app/src/main/java/com/lianluo/interview/reciver/DownloadBroadcast.java
@@ -0,0 +1,17 @@
+package com.lianluo.interview.reciver;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+
+/**
+ * Created by Administrator on 2018/2/26.
+ */
+
+public class DownloadBroadcast extends BroadcastReceiver {
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+
+    }
+}
---
+++ app/src/main/java/com/lianluo/interview/service/DownloadService.java
@@ -0,0 +1,17 @@
+package com.lianluo.interview.service;
+
+import android.app.Service;
+import android.content.Intent;
+import android.os.IBinder;
+
+/**
+ * Created by Administrator on 2018/2/26.
+ */
+
+public class DownloadService extends Service {
+
+    @Override
+    public IBinder onBind(Intent intent) {
+        return null;
+    }
+}
